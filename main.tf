b8ec4f1fd431b6eb436e636f6ceefd6887d4dc50
tabbertj <101358806+tabbertj@users.noreply.github.com>
2023-12-15 12:55:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,13 +17,7 @@ provider "azurerm" {
 }
 
 
-
-resource "azurerm_resource_group" "jt-tfe" {
-  name     = "tfe-pre-reqs"
-  location = "canadacentral"
-}
-
 resource "azurerm_dns_zone" "example-public" {
   name                = "azure.jtabbert.sbx.hashidemos.io"
-  resource_group_name = azurerm_resource_group.jt-tfe.name
+  resource_group_name = "tfe-azure"
 }
