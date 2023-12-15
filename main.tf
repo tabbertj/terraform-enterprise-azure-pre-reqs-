b806400ac4380cb906f40d04b973ab28f10b3524
tabbertj <101358806+tabbertj@users.noreply.github.com>
2023-12-15 09:27:35 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "tfe-pre-reqs"
+  location = "canadacentral"
+}
+
+resource "azurerm_dns_zone" "example-public" {
+  name                = "azure.jtabbert.sbx.hashidemos.io"
+  resource_group_name = azurerm_resource_group.example.name
+}
